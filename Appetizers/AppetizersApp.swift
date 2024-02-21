e5cfe3a0cd871978749a48ffd691cae5818eaebe
Matt Watters <mwatt023@uottawa.ca>
2024-02-21 17:33:28 -0500

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Matt Watters on 2024-02-21.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
